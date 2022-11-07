480760972b7ba90e918cbcc643ace44a375245cb
Lucas Signorelli <signorellilucas7@gmail.com>
2022-11-07 11:14:58 +0100

    Ajout vendor

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'netvod/src',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5d0860133cf140e3c5fc93fd0835cdb0d330a9d1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'netvod/src' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5d0860133cf140e3c5fc93fd0835cdb0d330a9d1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
