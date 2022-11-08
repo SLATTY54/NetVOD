5fec6df02e86fbcaa9f7424e0e2d69697131bd4e
kinzelinremy <kinzelinr@gmail.com>
2022-11-08 14:20:16 +0100

    update login style

--- src/actions/ActionLogin.php
+++ src/actions/ActionLogin.php
@@ -18,39 +18,7 @@ class ActionLogin extends Action
 
         if ($_SERVER['REQUEST_METHOD']=='GET'){
 
-            $html .= <<<HEREDOC
-            
-                <body>
-                <form method="post" action="?action=login">
-                    <video autoplay muted loop id="trailer">
-                        <source src="../resources/netvod_trailer.mov" type="video/mp4"></video>
-                    <img id="logo" src="../resources/logo.png" alt="logo">
-                    
-                    <div class="connexion">
-                       
-                        <div class="login">
-                            <h1 id="title">Se connecter</h1>
-                           
-                            <div class="emailControl">
-                                    <input type="email" name="email" id="id_email" placeholder="E-mail" class="textfield">      
-                            </div>
-                            
-                            <div class="passwordControl">
-                                    <input type="password" name="password" id="id_password" placeholder="Mot de passe" class="textfield">
-                            </div>
-                            <div class="buttonControl">
-                              <button type="submit" class="btnConnect">Connexion</button>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    </form>
-                    <p class="footer">@copyright KINZELIN Rémy,SIGNORELLI LUCAS, HIRTZ Jules, PERROT Alexandre and Lucas</p>
-                
-                    
-                    
-                
-            HEREDOC;
+            $html .= $this->renderHtml(false);
         }else{
             if (filter_var($_POST['email'],FILTER_VALIDATE_EMAIL) && filter_var($_POST['password'],FILTER_SANITIZE_STRING)){
                 $email = filter_var($_POST['email'],FILTER_VALIDATE_EMAIL);
@@ -59,16 +27,12 @@ class ActionLogin extends Action
                     Authentification::authenticate($email,$password);
                     echo "Vous êtes connecté";
                 }catch (AuthException $e){
-                    $html.= <<<HEREDOC
-                        <label style="color: white">Erreur de connexion l'adresse email ou le mot de passe est incorrect </label>
-                    HEREDOC;
+                    $html.= $this->renderHtml(true);
 
                 }
 
             }else{
-                $html.= <<<HEREDOC
-                        <label style="color: white">Erreur de connexion l'adresse email ou le mot de passe est incorrect </label>
-                    HEREDOC;
+                $html.= $this->renderHtml(true);
             }
 
         }
@@ -76,6 +40,54 @@ class ActionLogin extends Action
                 </body>
                 </html>
             HEREDOC;
+        return $html;
+    }
+
+
+
+    public function renderHtml(bool $error):string{
+
+            $html = <<<HEREDOC
+                    <body>
+                    <form method="post" action="?action=login">
+                        <video autoplay muted loop id="trailer">
+                            <source src="../resources/netvod_trailer.mov" type="video/mp4"></video>
+                        <img id="logo" src="../resources/logo.png" alt="logo">
+                        
+                        <div class="connexion">
+                           
+                            <div class="login">
+                                <h1 id="title">Se connecter</h1>
+                               
+                                <div class="emailControl">
+                                        <input type="email" name="email" id="id_email" placeholder="E-mail" class="textfield">      
+                                </div>
+                                
+                                <div class="passwordControl">
+                                        <input type="password" name="password" id="id_password" placeholder="Mot de passe" class="textfield">
+                                </div>
+                   HEREDOC;
+                    if ($error){
+
+                        $html.= <<<END
+                                    <div class="errorMessage">
+                                        <label>L'adresse E-mail ou le mot de passe est incorrect </label>
+                                    </div>
+                                    END;
+                    }
+                     $html.= <<<HEREDOC
+                                    <div class="buttonControl">
+                                      <button type="submit" class="btnConnect">Connexion</button>
+                                    </div>
+                                    <div class="Signup">
+                                        <p>Vous n'avez pas de compte ? <a href="#" style="color: red" >S'inscrire</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            </form>
+                            <p class="footer">@copyright KINZELIN Rémy,SIGNORELLI LUCAS, HIRTZ Jules, PERROT Alexandre and ERPELDING Lucas</p>
+                     HEREDOC;
         return $html;
     }
 }
