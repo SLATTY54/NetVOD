b60becd67d24b569e5995ad9cf389e1665532bfb
kinzelinremy <kinzelinr@gmail.com>
2022-11-08 22:00:59 +0100

    update

--- src/actions/ActionNoteCommentaire.php
+++ src/actions/ActionNoteCommentaire.php
@@ -4,6 +4,8 @@ namespace netvod\actions;
 
 class ActionNoteCommentaire extends Action {
 
+
+
         public function execute(): string {
             $html = <<<HEREDOC
                 <html lang="en">
@@ -17,7 +19,14 @@ class ActionNoteCommentaire extends Action {
             if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 $html .= $this->renderHtml(true);
             }else{
-                $html .= $this->renderHtml(true);
+                if (isset($_POST['send'])){
+                    $nbetoile = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+                    $commentaire = filter_var($_POST['commentaire'], FILTER_SANITIZE_STRING);
+                    $iduser = filter_var($_SESSION['user']->id, FILTER_SANITIZE_NUMBER_INT);
+                    echo $iduser;
+                    echo $nbetoile;
+                    echo $commentaire;
+                }
             }
 
             return $html;
@@ -25,19 +34,40 @@ class ActionNoteCommentaire extends Action {
 
         public function renderHtml(bool $popup):string{
 
-                $html = <<<HEREDOC
+            $nbEtoiles =0;
+            $html = <<<HEREDOC
                     <body>
-                            
                                 <div class="container">
                                     <button type="submit" onclick="openPopup()">click me!</button>
                                     <div class="popup" id="popup">
-                                        <h1>Noter le film</h1>
-                                        <button type="button" onclick="closePopup()">close</button>
+                                        <form method="post" >
+                                            <div class="popupContent">
+                                                <h1>Noter le film</h1>
+                                                <div class="rating">
+                                                   <a id="5" title="Donner 5 étoiles">☆</a>
+                                                   <a id="4" title="Donner 4 étoiles">☆</a>
+                                                   <a id="3" title="Donner 3 étoiles">☆</a>
+                                                   <a id="2" title="Donner 2 étoiles">☆</a>
+                                                   <a id="1" title="Donner 1 étoile">☆</a>
+                                                </div>
+                                                
+                                                    <div class="avis">
+                                                        <textarea class="commentaire" name="commentaire" id="commentaire" cols="30" rows="10" placeholder="Commentaire"></textarea>
+                                                    </div>
+                                                
+
+                                                    <button type="submit" name="send" class="btnE">Envoyer</button>
+                                                
+                                            </div>
+                                        </form>
+                                        <button type="button" class="btnC" onclick="closePopup()">Fermer</button>
                   
                                     </div>
-                                </div>
-                                
+                                </div>    
+                                                                
                                 <script>
+                                        
+                                
                                         let popup = document.getElementById("popup");
                                         function openPopup() {
                                             popup.classList.add("open-popup");
@@ -52,6 +82,7 @@ class ActionNoteCommentaire extends Action {
                         </body>
                     </html>
                 HEREDOC;
+
 
 
 
