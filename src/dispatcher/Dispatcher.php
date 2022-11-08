e84a6f5db68fdc89a1f2e2e69da5be66f11afdb3
Lucas <lucasNPC>
2022-11-08 16:47:33 +0100

    Ajout DisplayCatalogueAction et modification Dispatcher

--- src/dispatcher/Dispatcher.php
+++ src/dispatcher/Dispatcher.php
@@ -2,6 +2,7 @@
 
 namespace netvod\dispatcher;
 
+use netvod\actions\DisplayCatalogueAction;
 use netvod\actions\WelcomeAction;
 use netvod\actions\ActionLogin;
 use netvod\actions\SignUpAction;
@@ -19,6 +20,10 @@ class Dispatcher
     public function run(): void
     {
         switch ($this->action) {
+            case'catalogue':
+                $act = new DisplayCatalogueAction();
+                $html = $act->execute();
+                break;
             case 'login':
                 $act = new ActionLogin();
                 $html = $act->execute();
