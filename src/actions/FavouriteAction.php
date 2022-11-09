a6e3ef9168d21795b3ab5296bb74dfdb263079d1
Alexandre Perrot <alexandre.perrot54@gmail.com>
2022-11-09 21:09:42 +0100

    add filter_var

--- src/actions/FavouriteAction.php
+++ src/actions/FavouriteAction.php
@@ -20,8 +20,8 @@ class FavouriteAction extends Action
             $user = unserialize($_SESSION['user']);
             $id_user = $user->__get("id");
 
-            // on récupère l'ID de la série via le POST
-            $id_serie = $_POST['serie_id'];
+            // on récupère l'ID de la série via le POST (bien évidemment filtré)
+            $id_serie = filter_var($_POST['serie_id'], FILTER_SANITIZE_NUMBER_INT);
 
             // si la série est déjà dans les favoris de l'utilisateur, retirer la série aux favoris sinon l'ajouter
             if (Favourite::isAlreadyFavourite($id_user, $id_serie)) {
