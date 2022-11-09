f55602e275f5718f106e83f78da65e7322d4acff
Lucas <lucasNPC>
2022-11-09 10:26:21 +0100

    Modification de DisplayPreferencesAction.php : Lorsque l'user n'a pas de préférences, un message est affiché

--- src/actions/DisplayPreferencesAction.php
+++ src/actions/DisplayPreferencesAction.php
@@ -16,20 +16,25 @@ class DisplayPreferencesAction extends Action{
                 <div class="preferences">
                     <h1>Vos Préférences</h1>
                     <div class="list">
-                        <ul>
+                        
             end;
             $db = ConnectionFactory::makeConnection();
             $stmt = $db->prepare("SELECT serie.titre as titre,serie.id as id FROM serie INNER JOIN preferences ON serie.id=preferences.id_serie WHERE preferences.id_user=?");
             $id=$user->id;
             $stmt->bindParam(1,$id);
             $stmt->execute();
-            foreach($stmt->fetchAll(PDO::FETCH_OBJ) as $row){
-                $html.=<<<end
+            if($stmt->rowCount()===0){
+                $html.="<h2>Vous n'avez aucune série dans vos préférences</h2>";
+            }else{
+                $html.="<ul>";
+                foreach($stmt->fetchAll(PDO::FETCH_OBJ) as $row){
+                    $html.=<<<end
                     <li><a href="?action=serie&serie_id=$row->id">$row->titre</a></li>
                 end;
+                }
+                $html.="</ul>";
             }
             $html.=<<<end
-                        </ul
                     </div>
                 </div>
             end;
