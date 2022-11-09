a8dc57044de0028b70012fc315323eb2fbacb6d7
Lucas E <lucas.erp54@gmail.com>
2022-11-09 23:46:12 +0100

    modification de DisplayCatalogueAction.php qui utilise un fichier css

--- src/actions/DisplayCatalogueAction.php
+++ src/actions/DisplayCatalogueAction.php
@@ -10,7 +10,12 @@ class DisplayCatalogueAction extends Action
 
     public function execute(): string
     {
-        $html = "<h1>Catalogue de Série</h1>";
+        $html = <<< end
+            <head>
+                <link href="./css/accueil.css" rel="stylesheet">
+            </head>
+            <h1>Catalogue de Série</h1>
+        end;
         $bd = ConnectionFactory::makeConnection();
         $query = $bd->prepare("SELECT id,titre,img FROM serie");
         $query->execute();
@@ -20,7 +25,7 @@ class DisplayCatalogueAction extends Action
             $titre = $row['titre'];
             $img = "../ressources/images/".$row['img'];
             $html .= <<<end
-                <div class=$titre>
+                <div class=titre>
                     <img src=$img href='?action=serie&serie_id=$id' width="300" height="200">
                     <br><a href='?action=serie&serie_id=$id'>$titre</a>
                     <a href="?action=favourite&id=$id">⭐</a>
