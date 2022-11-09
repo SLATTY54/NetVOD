30f9e7d028c6f0e1374f45a754f35f05503122cc
Lucas <lucasNPC>
2022-11-09 17:06:46 +0100

    modif css

--- src/actions/RetirerFavorisAction.php
+++ src/actions/RetirerFavorisAction.php
@@ -7,8 +7,16 @@ use netvod\database\ConnectionFactory;
 class RetirerFavorisAction extends Action{
 
     public function execute(): string{
+        $html=<<< end
+            <html lang="en">
+                <head>
+                    <link href='./css/retrait.css' rel="stylesheet">
+                    <title>Retrait</title>
+                </head>
+                <body>
+        end;
         if(!isset($_SESSION['user'])){
-            $html="<h1><a href='?action=login'>Veuillez vous connecter</a></h1>";
+            $html.="<h1><a href='?action=login'>Veuillez vous connecter</a></h1></body></html>";
         }else{
             $pdo=ConnectionFactory::makeConnection();
             $user = unserialize($_SESSION['user']);
@@ -17,8 +25,14 @@ class RetirerFavorisAction extends Action{
             $stmt->bindParam(1,$id);
             $stmt->bindParam(2,$_GET['serie_id']);
             $stmt->execute();
-            $html="<h1>Serie retirée des favoris avec succès</h1>";
-            $html.="<a href='?action=accueil'>Retour à l'accueil</a>";
+            $html.=<<<end
+                <div class="retrait">
+                    <h1>Serie retirée des favoris avec succès</h1>
+                    <a href='?action=accueil'>Retour à l'accueil</a>
+                </div>
+                </body>
+                </html>
+            end;
         }
         return $html;
     }
