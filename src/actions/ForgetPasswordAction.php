e235f32039a061e70d9c4f76289b5b4386673c26
Lucas Signorelli <signorellilucas7@gmail.com>
2022-11-10 16:06:06 +0100

    ForgetPasswordAction.php correction du header (et un peu plus)

--- src/actions/ForgetPasswordAction.php
+++ src/actions/ForgetPasswordAction.php
@@ -2,12 +2,13 @@
 
 namespace netvod\actions;
 
+
 use netvod\classes\Authentification;
+use netvod\classes\Token;
+use netvod\database\ConnectionFactory;
 
 class ForgetPasswordAction extends Action
 {
-
-    private $token;
 
 
     public function execute(): string
@@ -21,28 +22,28 @@ class ForgetPasswordAction extends Action
             </head>
         HTML;
 
-        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $html .= $this->renderHtml(false, false);
+        } else {
 
-            $html .= $this->renderHtml(false);
-        }
-        else
-        {
-            if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
-            {
+            if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
                 $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-                if (Authentification::isRegistered($email))
-                {
-                    // TODO : export this to a token generation static function (in Authentification class ? or in a new class ?)
-                    $this->token = openssl_random_pseudo_bytes(8);
-                    $this->token = bin2hex($this->token);
 
-                    // TODO : redirect to a page to change password
-                    $html .= $this->renderChangingPassword($this->token);
+                if (Authentification::isRegistered($email)) {
+                    // TODO extraire les 5 lignes suivantes dans une fonction
+                    $db = ConnectionFactory::makeConnection();
+                    $stmt = $db->prepare('SELECT id FROM User WHERE email = ?');
+                    $stmt->execute([$email]);
+                    $id_user = $stmt->fetch()['id'];
+                    $stmt->closeCursor();
 
-                }
-                else
-                {
-                    $html .= $this->renderHtml(true);
+                    // genere le token et l'ajoute dans la base de données
+                    $token = Token::generateToken();
+                    Token::addTokenToUser($id_user, $token);
+                    header('Location: ?action=resetpassword&token=' . $token);
+
+                } else {
+                    $html .= $this->renderHtml(true, false);
                 }
             }
         }
@@ -51,13 +52,12 @@ class ForgetPasswordAction extends Action
     }
 
 
-    public function renderHtml(bool $errorEmail): string
+    public function renderHtml(bool $errorEmail, bool $errorPwd): string
     {
         $html = <<<HEREDOC
                 <body>
-                <form method="post" action="?action=forget_password&token={$this->token}"> 
-                <video autoplay muted loop id="trailer">
-                    <source src="../resources/netvod_trailer.mp4" type="video/mp4"></video>
+                <form method="post" action="?action=forgetPassword">
+                
                 <img id="logo" src="../resources/logo.png" alt="logo">
                 
                 <div class="connexion">
@@ -67,37 +67,38 @@ class ForgetPasswordAction extends Action
                         <div class="emailControl">
                             <input type="email" name="email" id="id_email" placeholder="E-mail" class="textfield">
                         </div>
+
                 HEREDOC;
 
-        if ($errorEmail) {
+        if ($errorEmail)
+        {
+            $html .= <<<HEREDOC
+                <div class="error">
+                    <p>L'adresse email n'est pas valide</p>
+                </div>
+                HEREDOC;
+        }
+
+        if ($errorPwd)
+        {
             $html .= <<<HEREDOC
                         <div class="errorMessage">
-                            <label>Cet email n'existe pas</label>
+                            <label>Les mots de passe ne correspondent pas</label>
                         </div>
                     HEREDOC;
         }
-        $html .= <<<HEREDOC
-                        
-                        <div class="buttonControl">
-                            <input type="submit" class="btnRegister" value="Mot de passe oublié">
-                        </div>
-                    </div>
-                </div>
-                </form>
-            </body>
-            </html>
-                
-        HEREDOC;
+
+        $html.= <<<HEREDOC
+                                    <div class="buttonControl">
+                                      <button type="submit" class="btnConnect">Changer le mot de passe</button>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            </form>
+                            <p class="footer">@copyright KINZELIN Rémy,SIGNORELLI LUCAS, HIRTZ Jules, PERROT Alexandre and ERPELDING Lucas</p>
+                     HEREDOC;
 
         return $html;
-    }
-
-
-    public function renderChangingPassword(string $token): string
-    {
-        // TODO : use this <a href="?action=forget_password&token={$token}"></a> to put the token in the url or line 58
-        $hmtl = "<p>$token</p>";
-
-        return $hmtl;
     }
 }
