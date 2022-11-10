0ab06b7326d5fb9cdaa0693fbb1629c362b7a0b9
jules hirtz <htz_j@protonmail.com>
2022-11-10 15:39:56 +0100

    css encore

--- src/actions/DisplayCommentaireAction.php
+++ src/actions/DisplayCommentaireAction.php
@@ -11,23 +11,42 @@ class DisplayCommentaireAction extends Action
     public function execute(): string
     {
         $html = <<<end
-                <div class="commentaire">
+            <html>
+            <head>
+            <title>NetVOD</title>
+            <link href="./css/coms-style.css" rel="stylesheet">
+                                      <link href="./css/Notation-style.css" rel="stylesheet">
+                          <meta name="viewport" content="width=device-with,initial-scale=1.0">
+
+            </head> 
+            <body>
+                <div class="titre">
                     <h1>Commentaires</h1>
-                    <div class="commentaires">
+                </div>  
+                    <div class="coms">
             end;
         $db = ConnectionFactory::makeConnection();
-        $stmt = $db->prepare("SELECT note, commentaire FROM notation WHERE id_serie = ?");
+        $stmt = $db->prepare("SELECT note, commentaire, nom, prenom FROM notation INNER JOIN User on id_user = id WHERE id_serie = ?");
         $serie = $_GET["serie"];
         $stmt->bindParam(1, $serie);
         $stmt->execute();
         foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
             $html .= <<<end
-                    <p>commentaire : {$row['commentaire']} <br>note : {$row['note']} <br><br></p>
+                
+                    <p>{$row['note']} sur 5 - {$row['prenom']} {$row['nom']}</p> 
+                   <div class="commentaire"> 
+                    <p>
+                    "{$row['commentaire']}" 
+                    </p>
+                    </div>
+                    <br>
                 end;
         }
         $html .= <<<end
                     </div>
-                </div>
+                
+            </body>
+            </html>
             end;
         return $html;
     }
